cb11573428f6ccb1bf80f7c74c7a7412e401e136
Camila Buldin <cami@Camilas-MacBook-Air.local>
2023-05-16 14:20:30 +0200

    habit pushed to user

--- Habit-Tracker/routes/user.routes.js
+++ Habit-Tracker/routes/user.routes.js
@@ -19,38 +19,57 @@ router.get("/profile", isLoggedIn, (req, res, next) => {
     .catch((err) => next(err))
 });
 
-router.get('/habit/create', (req, res, next) => {
-    res.render('createHabit', {layout: 'layouts/layout2'})
+router.get('/habit/create', isLoggedIn, (req, res, next) => {
+    res.render('createHabit', {layout: 'layout'})
 })
 
 router.post('/habit/create', (req, res, next) => {
-    const {title, description, private, addUsers} = req.body
-    const newHabit = {
-        title,
-        userId: req.session.currentUser._id,
-        description,
-        datesCompleted: [],
-        groupOfUsers: [], // Array of User IDs
-        private
-      }
-    Habit.create(newHabit)
-      .then(habit => {
-        console.log('New habit saved:', habit);
-        res.redirect('/profile');
-      })
-          
-      .catch(err => {
-        next(err)
-      })
-    })  
+  const {title, description, private, addUsers} = req.body
+  const newHabit = {
+    title,
+    userId: req.session.currentUser._id,
+    description,
+    datesCompleted: [],
+    groupOfUsers: [], // Array of User IDs
+    private
+  }
+  Habit.create(newHabit)
+  .then(habit => {
+    console.log('New habit saved:', habit);
+    return User.findByIdAndUpdate(req.session.currentUser._id, { $push: { habits: habit._id }})
+    // return User.findById(req.session.currentUser._id)
+  })
+  
+  .then((user) => {
+    res.redirect('/profile');
+    // let updatedHabit = user.habits.push(habit._id)
+   })
+
+  .then((userInfo) => {
+      console.log(userInfo)
+     }) 
+
+  .catch(err => {
+    next(err)
+   })
+  })  
 
 
-router.get(":username/profile/edit", isLoggedIn, (req, res, next) => {
+router.get("/profile/edit", isLoggedIn, (req, res, next) => {
     User.findOne(req.session.currentUser)
     .then(user => {
         res.render("edit-profile", user)
     })
     .catch((err) => next(err))
 })
+
+router.get('/group-list', isLoggedIn, (req, res, next) => {
+    res.render('groupList', {layout: 'layout'})
+})
+
+router.get('/:id/habit', isLoggedIn, (req, res, next) => {
+  res.render('groupHabit', {layout: 'layout'})
+})
+
 
 module.exports = router;
